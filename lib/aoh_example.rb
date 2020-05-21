c06305edc4d92c489a55023a768d761b36611e18
Dennis Portillo <dennis.portillo@yale.edu>
2020-05-21 16:28:55 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -51,5 +51,5 @@ def aoh_update(aoh, row, key, new_value)
     { name:  "Kate Gompert", occupation: "None" },
     { name:  "Bruce Green", occupation: "Fan of Mildred" }
     ]
-    aoh_update[1][:name] = "Joelle van Dyne (PGOAT)"
+    aoh_update[][:name] = "Joelle van Dyne (PGOAT)"
 end
