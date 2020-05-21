5665dd759d8c1d33f99011a360a15a3d1f9d8c9b
Dennis Portillo <dennis.portillo@yale.edu>
2020-05-21 16:24:54 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -44,6 +44,12 @@ end
 def aoh_update(aoh, row, key, new_value)
   # Update the AoH data at row and key to have the value of new_value
   # Return the updated AoH
-  aoh_update = 
+  aoh_update = [
+    { name:  "Don Gately", occupation: "Live-in Staff" },
+    { name:  "Joelle van Dyne", occupation: "Radio Personality" },
+    { name:  "Pat Monteseian", occupation: "Staff" },
+    { name:  "Kate Gompert", occupation: "None" },
+    { name:  "Bruce Green", occupation: "Fan of Mildred" }
+    ]
     aoh_update[1][1] = "Joelle van Dyne (PGOAT)"
 end
